aa014109d5ce0a4efb6422f495c63431d0f4d7a5
Glib ZAGURA <gzagura@e2r6p2.unit.ua>
2018-06-07 18:02:21 +0300

    damn

--- get_next_line.c
+++ get_next_line.c
@@ -19,22 +19,39 @@
 int		main(int argc, char const *argv[])
 {
 	int fd;
-	//printf("LOX\n");
 	fd = open("text.txt", O_RDONLY);
 	get_next_line(fd,NULL);
 	return 0;
 }
-void	ft_newlist(t_getlist *head, int fd)
+
+char	*ft_strjoinfree(char *s1, char *s2)
+{
+	char *string;
+
+	string = NULL;
+	string = (char *)malloc(sizeof(char) * (ft_strlen(s1) +
+											ft_strlen(s2) + 1));
+	if (!string)
+		return (NULL);
+	ft_strcpy(string, s1);
+	ft_strcpy(string + ft_strlen(s1), s2);
+	return (string);
+}
+t_getlist	*ft_newlist(t_getlist *head, int fd)
 {
 	head = (t_getlist*)malloc(sizeof(t_getlist));
 	head->next = NULL;
-	head->content = ft_strnew(BUFF_SIZE);
+	head->content = (char*)malloc(sizeof(BUFF_SIZE + 1));
 	head->num_fd = fd;
+	return (head);
 }
 t_getlist	*ft_worklist(t_getlist *head, int fd)
 {
 	if (head == NULL)
-			ft_newlist(head, fd);
+	{
+		head = ft_newlist(head, fd);
+		return (head);
+	}
 	while (head)
 	{
 		if (head == NULL)
@@ -50,12 +67,17 @@ int		ft_crazy(char *buffe, int fd)
 {
 	t_getlist *head;
 	int i = -1;
+	char *line;
 
-	if (!head)
-		ft_worklist(head, fd);
-	head->content = ft_strjoin(head->content, buffe);
+	line = NULL;
+	head = NULL;
+	head = ft_worklist(head, fd);
+	line = ft_strjoinfree(head->content, buffe);
+	printf("%s\n",line);
+	printf("CYKA WORKS\n");
 	while (head->content[++i] != '\0')
 	{
+		
 		if(head->content[i] == '\n')
 		{
 			printf("CRAZY: %s\n",head->content);
@@ -70,10 +92,14 @@ int		get_next_line(const int fd, char **line)
 	int		bytes = 1;
 	char	buffe[BUFF_SIZE + 1];
 
-	while (ft_crazy(buffe, fd) == 1 && bytes > 0)
+	while (bytes > 0)
 	{
 		bytes = read(fd, buffe, BUFF_SIZE);
 		ft_crazy(buffe, fd);
+		if (ft_crazy(buffe, fd) == 1)
+		{
+			return (0);
+		}
 	}
 	return (0);
 }
