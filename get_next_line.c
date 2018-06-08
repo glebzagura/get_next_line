d308500e83265635638f53a8246ccd0224886d6a
Glib ZAGURA <gzagura@e2r8p1.unit.ua>
2018-06-08 18:53:57 +0300

    suka

--- get_next_line.c
+++ get_next_line.c
@@ -19,34 +19,18 @@
 int		main(int argc, char const *argv[])
 {
 	int fd;
-	char lol[] = "lol\ntu dibil";
-	char *lol1;
 
-	lol1 = ft_strrchr(lol, '\n');
-	printf("!!!!! ____>   %s\n",lol1 );
 	fd = open("text.txt", O_RDONLY);
 	get_next_line(fd,NULL);
 	return 0;
 }
 
-char	*ft_strjoinfree(char *s1, char *s2)
-{
-	char *string;
 
-	string = NULL;
-	string = (char *)malloc(sizeof(char) * (ft_strlen(s1) +
-											ft_strlen(s2) + 1));
-	if (!string)
-		return (NULL);
-	ft_strcpy(string, s1);
-	ft_strcpy(string + ft_strlen(s1), s2);
-	return (string);
-}
 t_getlist	*ft_newlist(t_getlist *head, int fd)
 {
 	head = (t_getlist*)malloc(sizeof(t_getlist));
 	head->next = NULL;
-	head->content = (char*)malloc(sizeof(BUFF_SIZE + 1));
+	head->content = (char*)malloc(sizeof(2));
 	head->num_fd = fd;
 	return (head);
 }
@@ -68,30 +52,36 @@ t_getlist	*ft_worklist(t_getlist *head, int fd)
 	return (head);
 }
 
-int		ft_crazy(char *buffe, int fd)
+int		ft_crazy(char *buffe, int fd, char **line)
 {
 	static t_getlist *head;
 	int i = -1;
-	char *line;
+	char suka[100];
 
 	line = NULL;
 	head = NULL;
 	head = ft_worklist(head, fd);
-	head->content = ft_strjoinfree(head->content, buffe);
-	printf("---------------\n");
+	suka = ft_strjoin(suka, buffe);
+	printf("BUFF ----> %s\n\n",buffe );
+	printf("SUKA ------> %s\n\n", suka);
+	head->content = ft_strjoin(head->content, buffe);
+	printf("-------FULL--------\n");
 	printf("%s\n",head->content);
 	printf("---------------\n");
-	printf("CYKA WORKS\n");
 	while (head->content[++i] != '\0')
 	{
 		
 		if(head->content[i] == '\n')
 		{
-			printf("CRAZY: %s\n\n",head->content);
+			printf("%s\n",head->content );
+			line = ft_strsub(head->content, 0, i);
+			printf("--------ANSWER DOWN--------\n");
+			printf("%s\n", line);
+			printf("--------------------\n\n");
 			return (1);
 		}
 	}
-	return (1);
+	return (0);
 }
 
 int		get_next_line(const int fd, char **line)
@@ -102,11 +92,27 @@ int		get_next_line(const int fd, char **line)
 	while (bytes > 0)
 	{
 		bytes = read(fd, buffe, BUFF_SIZE);
-		ft_crazy(buffe, fd);
-		if (ft_crazy(buffe, fd) == 1)
+		if (ft_crazy(buffe, fd, line) == 1)
 		{
 			return (0);
 		}
 	}
 	return (0);
 }
+
+
+
+
+// char	*ft_strjoinfree(char *s1, char *s2)
+// {
+// 	char *string;
+
+// 	string = NULL;
+// 	string = (char *)malloc(sizeof(char) * (ft_strlen(s1) +
+// 											ft_strlen(s2) + 1));
+// 	if (!string)
+// 		return (NULL);
+// 	ft_strcpy(string, s1);
+// 	ft_strcpy(string + ft_strlen(s1), s2);
+// 	return (string);
+// }
