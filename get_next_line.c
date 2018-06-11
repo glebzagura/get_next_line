3e7bc54c316441c861964da413f17d99f03ccd91
Glib ZAGURA <gzagura@e2r8p3.unit.ua>
2018-06-11 17:05:54 +0300

    работает не трож

--- get_next_line.c
+++ get_next_line.c
@@ -18,19 +18,36 @@
 
 int		main(int argc, char const *argv[])
 {
-	int fd;
+	int fd1;
+	char *line;
 
-	fd = open("text.txt", O_RDONLY);
-	get_next_line(fd,NULL);
+	fd1 = open("text", O_RDONLY);
+	get_next_line(fd1, &line);
+	printf("after gnl = %s\n", line);
+	get_next_line(fd1, &line);
+	printf("after gnl = %s\n", line);
 	return 0;
 }
+char	*ft_strjoinfree(char *s1, char *s2)
+{
+	char *string;
 
+	string = NULL;
+	string = (char *)malloc(sizeof(char) * (ft_strlen(s1) +
+											ft_strlen(s2) + 1));
+	if (!string)
+		return (NULL);
+	ft_strcpy(string, s1);
+	ft_strcpy(string + ft_strlen(s1), s2);
+	return (string);
+}
 
 t_getlist	*ft_newlist(t_getlist *head, int fd)
 {
 	head = (t_getlist*)malloc(sizeof(t_getlist));
 	head->next = NULL;
-	head->content = (char*)malloc(sizeof(2));
+	head->content = (char*)malloc(sizeof(1));
+	ft_bzero(head->content, 2);
 	head->num_fd = fd;
 	return (head);
 }
@@ -43,9 +60,7 @@ t_getlist	*ft_worklist(t_getlist *head, int fd)
 	}
 	while (head)
 	{
-		if (head == NULL)
-			ft_newlist(head, fd);
-		else if(head->num_fd == fd)
+		if(head->num_fd == fd)
 			return (head);
 		head = head->next;
 	}
@@ -54,17 +69,12 @@ t_getlist	*ft_worklist(t_getlist *head, int fd)
 
 int		ft_crazy(char *buffe, int fd, char **line)
 {
-	static t_getlist *head;
+	static t_getlist *head = NULL;
 	int i = -1;
-	char suka[100];
 
-	line = NULL;
-	head = NULL;
+	*line = NULL;
 	head = ft_worklist(head, fd);
-	suka = ft_strjoin(suka, buffe);
-	printf("BUFF ----> %s\n\n",buffe );
-	printf("SUKA ------> %s\n\n", suka);
-	head->content = ft_strjoin(head->content, buffe);
+	head->content = ft_strjoinfree(head->content, buffe);
 	printf("-------FULL--------\n");
 	printf("%s\n",head->content);
 	printf("---------------\n");
@@ -73,11 +83,11 @@ int		ft_crazy(char *buffe, int fd, char **line)
 		
 		if(head->content[i] == '\n')
 		{
-			printf("%s\n",head->content );
-			line = ft_strsub(head->content, 0, i);
-			printf("--------ANSWER DOWN--------\n");
-			printf("%s\n", line);
-			printf("--------------------\n\n");
+			*line = ft_strsub(head->content, 0, i);
+			printf("len = %d\n", (int)ft_strlen(head->content));
+			printf("head->content = %s\n", head->content + i + 1);
+			head->content = ft_strsub(head->content + i + 1, 0, ft_strlen(head->content + i + 1));
+			printf("CCContent = %s\n\n",head->content);
 			return (1);
 		}
 	}
@@ -94,25 +104,10 @@ int		get_next_line(const int fd, char **line)
 		bytes = read(fd, buffe, BUFF_SIZE);
 		if (ft_crazy(buffe, fd, line) == 1)
 		{
+			printf("line =  %s\n", *line);
 			return (0);
 		}
 	}
 	return (0);
 }
 
-
-
-
-// char	*ft_strjoinfree(char *s1, char *s2)
-// {
-// 	char *string;
-
-// 	string = NULL;
-// 	string = (char *)malloc(sizeof(char) * (ft_strlen(s1) +
-// 											ft_strlen(s2) + 1));
-// 	if (!string)
-// 		return (NULL);
-// 	ft_strcpy(string, s1);
-// 	ft_strcpy(string + ft_strlen(s1), s2);
-// 	return (string);
-// }
